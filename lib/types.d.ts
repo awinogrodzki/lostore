da1ab0b1e0b8d4043f6f8bd9b8dd03bff344841f
Amadeusz Winogrodzki <a.winogrodzki@ocado.com>
2020-02-14 03:23:45 +0100

    chore: build

--- lib/types.d.ts
+++ lib/types.d.ts
@@ -1,5 +1,5 @@
 /// <reference types="react" />
-export declare type ActionReducer<S, RS> = (state: S, rootState?: RS) => S;
+export declare type ActionReducer<S, RS> = (state: S, rootState: RS) => S;
 export declare type ActionReducerCreator<S = any, RS = S, T extends (...args: any[]) => ActionReducer<S, RS> | AsyncActionReducer<S, RS> = any> = (...args: Parameters<T>) => ReturnType<T>;
 export declare type ActionReducers<S extends any = any, T extends ActionReducers<S> = any, RS = S> = {
     [K in keyof T]: T[K] extends ActionReducers<S[K]> ? ActionReducers<S[K], T[K], S> : T[K] extends ActionReducerCreator<S[K]> ? ActionReducerCreator<S[K], S, T[K]> : never;
@@ -11,10 +11,10 @@ export declare type Actions<S extends any, T extends any, RS = S> = {
 export interface StoreProviderProps<S> {
     initialState?: S;
 }
-export declare type SetState<S> = React.Dispatch<React.SetStateAction<S>>;
+export declare type SetState<S, RS> = (callback: (prevState: S, rootState: RS) => S) => void;
 export interface StoreContextValue<S> {
     state: S;
-    setState: SetState<S>;
+    setState: React.Dispatch<React.SetStateAction<S>>;
 }
 export declare type StoreContext<S> = React.Context<StoreContextValue<S>>;
 export declare type AsyncActionReducer<S, RS> = Promise<ActionReducer<S, RS>>;
