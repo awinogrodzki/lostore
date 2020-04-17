3f5a78b2568e30f3af1974c3e73b413017bddfe9
Amadeusz Winogrodzki <a.winogrodzki@ocado.com>
2020-04-17 12:14:25 +0200

    chore: release

--- lib/provider.d.ts
+++ lib/provider.d.ts
@@ -1,4 +1,3 @@
 import { StoreContext, StoreProviderProps } from './types';
 import * as React from 'react';
-export declare const isStateEqual: <S extends any>(prevState: S, nextState: S) => boolean;
 export declare const createStoreProvider: <S extends any, T>(StoreContext: StoreContext<S>) => React.FunctionComponent<StoreProviderProps<S>>;
--- lib/utils.d.ts
+++ lib/utils.d.ts
@@ -1,1 +1,2 @@
 export declare const isObjectLiteral: (value: any) => value is object;
+export declare const isStateEqual: <S extends any>(prevState: S, nextState: S) => boolean;
